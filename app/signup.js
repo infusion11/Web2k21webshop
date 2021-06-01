2343421095ed4ba0cde39d8aa3d1e2356733438e
Molnár Rajmund <inspiration15x@gmail.com>
2021-06-01 15:55:30 +0200

    email regex egyszerűsítés

--- app/signup.js
+++ app/signup.js
@@ -149,8 +149,9 @@ function setSuccessMessage(input){
 }
 
 function checkMail(email){
-    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+    return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]{2,})*$/.test(email);
 }
+
 function checkPw(password){
     return /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])/.test(password);
 }
